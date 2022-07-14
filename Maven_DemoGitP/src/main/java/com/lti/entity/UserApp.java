f85fe276fe33e9db8d182ef804f00f2cdaec4683
gayatriuh <gayatriumesh34@gmail.com>
2022-07-14 12:25:15 +0530

    second commit

--- Maven_DemoGitP/src/main/java/com/lti/entity/UserApp.java
+++ Maven_DemoGitP/src/main/java/com/lti/entity/UserApp.java
@@ -2,6 +2,7 @@ package com.lti.entity;
 
 public class UserApp {
 	public static void main(String[] args) {
+		System.out.println("Welecome to Calculator");
 		Integer.parseInt("One");
 		Calculator c=new Calculator();
 		int r=c.add(100,300);
